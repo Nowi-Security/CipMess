d6c77213b906aaa73e6f12aceb1c43e89aa70183
Tirsi <tirsi333@gmail.com>
2025-08-29 20:20:49 +0200

    New security update

--- app/src/main/java/com/cipmess/MainActivity.kt
+++ app/src/main/java/com/cipmess/MainActivity.kt
@@ -232,6 +232,8 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
